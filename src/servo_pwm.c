eccf6cc02d557c40a831fd3ed98124aa483c510e
Marcus Luck <marcus.luck@outlook.com>
2023-08-14 12:09:02 -1000

    ADD: better queue code

--- src/servo_pwm.c
+++ src/servo_pwm.c
@@ -20,52 +20,17 @@
 #define argh GPIO_FUNC_UART
 
 
-#define LED_PIN      3
-#define PWM_PIN      4
-#define PWM_PIN2     5
+#define PWM_PIN0     2
+#define PWM_PIN1     3
+#define PWM_PIN2     4
+#define PWM_PIN3     5
+
+int LED_BANK[] = {PWM_PIN0, PWM_PIN1, PWM_PIN2, PWM_PIN3};
+
+#define LEN_LED_BANK 4
 
 
-void vPWMTask() {
-    static int fade = 0;
-    static bool going_up = true;
 
-   for (;;) {
-      if (going_up) {
-         ++fade;
-         if (fade > 255) {
-            fade = 255;
-            going_up = false;
-         }
-      } else {
-         --fade;
-         if (fade < 0) {
-            fade = 0;
-            going_up = true;
-         }
-      }
-
-      pwm_set_gpio_level(PWM_PIN2, fade * fade);
-
-      vTaskDelay(10);
-   }
-}
-
-void vBlinkTask() {
-
-   for (;;) {
-
-      gpio_put(LED_PIN, 1);
-
-      vTaskDelay(250);
-
-      gpio_put(LED_PIN, 0);
-
-      vTaskDelay(250);
-
-   }
-}
-
-QueueHandle_t cmd_queue;
 QueueHandle_t pwmQueue;
 
 uint16_t cdiv = 12000;
@@ -73,11 +38,17 @@ uint16_t pwm_val = 12000;
 
 
 void vRunPWMTask() {
-   pwm_set_gpio_level(PWM_PIN, cdiv);
+   // for (int i=0; i<LEN_LED_BANK; i++) {
+   //    pwm_output_init(LED_BANK[i], 4.f, 32000);
+   // }
    for (;;) {
-      if (xQueueReceive(pwmQueue, &pwm_val, 0) == pdTRUE) {
+      
+      if (xQueueReceive(pwmQueue, (void *) &pwm_val, 10) == pdTRUE) {
          printf("Received: %d\n", pwm_val);
-         pwm_set_gpio_level(PWM_PIN, pwm_val);
+         for (int i=0; i<LEN_LED_BANK; i++) {
+            pwm_set_gpio_level(LED_BANK[i], pwm_val);
+         }
+         printf("Updated Duty cycles\n");
       }
       vTaskDelay(10);  
    }
@@ -114,12 +85,12 @@ void vParseCommandTask() {
    uint8_t cmd_type;
    uint16_t val;
    for (;;) {
-      if (xQueueReceive(cmd_queue, &cmdbuff, 100) == pdTRUE) {
+      if (cmd_queue_read(&cmdbuff) == pdTRUE) {
          cmd_type = cmdbuff.cmd_type;
          val = cmdbuff.val;
          if (cmd_type == 0) {
             printf("setting cdiv\n");
-            xQueueSendToFront(pwmQueue, &val, 0);
+            xQueueSendToFront(pwmQueue, val, 0);
          } else if (cmd_type == 1) {
             uint8_t pin = val & 0xF;
             printf("turning on %i\n", pin);
@@ -138,31 +109,24 @@ void vParseCommandTask() {
 void main() {
    // Initialize the Queue
    pwmQueue =  xQueueCreate( 1, sizeof( uint16_t ) );
-   cmd_queue = xQueueCreate( 1, sizeof( command_frame_t ) );
    // Set up our UART with the required speed.
    stdio_init_all();
-   printf("Hello, world!\n");
+   printf("#### Starting program ####\n");
 
-   init_uart(cmd_queue);
-
-   // task parse commands
-
-
-   // Setup pin for blink
-   gpio_init(LED_PIN);
-   gpio_set_dir(LED_PIN, GPIO_OUT);
-
-   int pin_bank[] = {PWM_PIN, PWM_PIN2};
+   init_uart();
 
    // Setup pin for PWM static
-   // gpio_init(PWM_PIN2);
-   pwm_output_init(pin_bank[0], 4.f, 32000);
+   printf("Setting up pins\n");
+   for (int i=0; i<LEN_LED_BANK; i++) {
+      gpio_init(LED_BANK[i]);
+      gpio_set_dir(LED_BANK[i], GPIO_OUT);
+      pwm_output_init(LED_BANK[i], 4.f, 32000);
+   }
 
-   // Setup pins for PWM
-   pwm_output_init(pin_bank[1], 4.f, 32000);
    // Create tasks
-   xTaskCreate(vBlinkTask, "Blink Task", 128, NULL, 2, NULL);
-   xTaskCreate(vPWMTask, "PWM Task", 128, NULL, 1, NULL);
+   printf("Creating tasks\n");
+   // xTaskCreate(vBlinkTask, "Blink Task", 128, NULL, 2, NULL);
+   // xTaskCreate(vPWMTask, "PWM Task", 128, NULL, 1, NULL);
 
    xTaskCreate(vRunPWMTask, "Run PWM Task", 128, NULL, 3, NULL);
    xTaskCreate(vParseCommandTask, "Parser Task", 128, NULL, 4, NULL);
--- src/uart.c
+++ src/uart.c
@@ -6,7 +6,26 @@
 #include "FreeRTOS.h"
 #include "queue.h"
 
-QueueHandle_t queue;
+#define QUEUE_LENGTH 1
+#define QUEUE_ITEM_SIZE sizeof(command_frame_t)
+#define QUEUE_TIMEOUT 10
+
+QueueHandle_t cmd_queue;
+
+// Setup command queue
+void cmd_queue_init() {
+   cmd_queue = xQueueCreate(QUEUE_LENGTH, QUEUE_ITEM_SIZE);
+}
+
+// Send command to queue
+void cmd_queue_send(command_frame_t * cmd) {
+   xQueueSendToBack(cmd_queue, (void *) &cmd, QUEUE_TIMEOUT);
+}
+
+// Read command from queue
+BaseType_t cmd_queue_read(command_frame_t * cmd) {
+    return xQueueReceive(cmd_queue, (void *) &cmd, QUEUE_TIMEOUT);
+}
 
 void read_cmd(char s[], char end) {
    for (int i=0; i<MAX_UART_MESSAGE_LEN; i++) {
@@ -23,32 +42,35 @@ void read_cmd(char s[], char end) {
 void on_uart_rx() {
    static int chars_rxed = 0;
    char cmd[] = {0,0,0,0};
+   while (1) {
+        while (uart_is_readable(UART_ID)) {
+            printf("UART Receiving.\n");
+            read_cmd(cmd, UART_END);
 
-    while (uart_is_readable(UART_ID)) {
-        read_cmd(cmd, UART_END);
+            // parse command
 
-        // parse command
+            _Bool rw = cmd[0] >> 7;
+            uint8_t cmd_type = cmd[0] & 0x7f;
+            uint8_t addr = cmd[1];
 
-        _Bool rw = cmd[0] >> 7;
-        uint8_t cmd_type = cmd[0] & 0x7f;
-        uint8_t addr = cmd[1];
+            uint16_t high = (int)cmd[2] << 8;
+            uint16_t low = (int)cmd[3];
+            uint16_t val = high + low;
+            printf("rw: %i, cmd: %i, addr: %i, val: %i\n", rw, cmd_type, addr, val);
 
-        uint16_t high = (int)cmd[2] << 8;
-        uint16_t low = (int)cmd[3];
-        uint16_t val = high + low;
-        printf("rw: %i, cmd: %i, addr: %i, val: %i\n", rw, cmd_type, addr, val);
+            command_frame_t cnd;
+            cnd.rw = rw;
+            cnd.cmd_type = cmd_type;
+            cnd.addr = addr;
+            cnd.val = val;
 
-        command_frame_t cnd;
-        cnd.rw = rw;
-        cnd.cmd_type = cmd_type;
-        cnd.addr = addr;
-        cnd.val = val;
-
-        xQueueSendToFront(queue, &cnd, 0);
+            cmd_queue_send(&cnd);
     }
+    vTaskDelay(10);
+   }
 }
 
-void init_uart(QueueHandle_t * q) {
+void init_uart() {
 
    uart_init(UART_ID, BAUD_RATE);
    // Set the TX and RX pins by using the function select on the GPIO
@@ -59,12 +81,14 @@ void init_uart(QueueHandle_t * q) {
    uart_set_hw_flow(UART_ID, false, false);
    // Set our data format
    uart_set_format(UART_ID, DATA_BITS, STOP_BITS, PARITY);
+
+    // Turn off FIFO's - we want to do this character by character
+    uart_set_fifo_enabled(UART_ID, false);
+
     // Set up a RX interrupt
     // We need to set up the handler first
     // Select correct interrupt for the UART we are using
     int UART_IRQ = UART_ID == uart0 ? UART0_IRQ : UART1_IRQ;
-
-    queue = q;
 
     // And set up and enable the interrupt handlers
     irq_set_exclusive_handler(UART_IRQ, on_uart_rx);
